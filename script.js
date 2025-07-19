52568b319d0f71597f81ba08767bae5ee7f42203
vrikri <krishnasaini197569@gmail.com>
2025-07-19 21:05:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("orderForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Product added to cart! Please click 'Pay Now' to complete your order.");
+});
